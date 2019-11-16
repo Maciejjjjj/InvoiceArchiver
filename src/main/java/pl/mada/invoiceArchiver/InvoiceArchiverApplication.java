64530cd360a369c53a6af901f3fdeb958f5b5b50
maciej <sucheckimaciej3@gmail.com>
2019-11-16 13:33:51 +0100

    Project initialized

---
+++ src/main/java/pl/mada/invoiceArchiver/InvoiceArchiverApplication.java
@@ -0,0 +1,13 @@
+package pl.mada.invoiceArchiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceArchiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceArchiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mada/invoiceArchiver/InvoiceArchiverApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mada.invoiceArchiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceArchiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
