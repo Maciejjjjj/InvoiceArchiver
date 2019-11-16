35ce3d3e403deb14dc5662652adcbebef55cbb3f
dannir19 <d.krauz@op.pl>
2019-11-16 14:42:44 +0100

    rename package

--- src/main/java/pl/mada/invoice_archiver/InvoiceArchiverApplication.java
+++ src/main/java/pl/mada/invoice_archiver/InvoiceArchiverApplication.java
@@ -1,4 +1,4 @@
-package pl.mada.invoiceArchiver;
+package pl.mada.invoice_archiver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/pl/mada/invoice_archiver/InvoiceArchiverApplicationTests.java
+++ src/test/java/pl/mada/invoice_archiver/InvoiceArchiverApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.mada.invoiceArchiver;
+package pl.mada.invoice_archiver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
