8e83de0a1d23a95805ca74c50b576617dd0ec232
piotrkazm <kazioks@gmail.com>
2019-11-17 11:14:34 +0100

    Entity4

---
+++ src/main/java/pl/mada/invoice_archiver/model/entities/File.java
@@ -0,0 +1,84 @@
+package pl.mada.invoice_archiver.model.entities;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+
+@Entity
+    @Table(name = "Files")
+    @Getter @Setter @ToString(exclude = {"data"})
+    public class File  {
+        @Id
+        @GeneratedValue(strategy= GenerationType.IDENTITY)
+        @Column(name="id")
+        private Long id;
+
+        @Column(name = "file_name", nullable = false)
+        private String fileName;
+        @Column(name = "content_type", nullable = false)
+        private String contentType;
+        @Lob
+        @Basic(fetch = FetchType.LAZY, optional = false)
+        @Column(name = "data", nullable = false, columnDefinition = "MEDIUMBLOB")
+        private byte[] data;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getFileName() {
+            return fileName;
+        }
+
+        public void setFileName(String fileName) {
+            this.fileName = fileName;
+        }
+
+        public String getContentType() {
+            return contentType;
+        }
+
+        public void setContentType(String contentType) {
+            this.contentType = contentType;
+        }
+
+        public byte[] getData() {
+            return data;
+        }
+
+        public void setData(byte[] data) {
+            this.data = data;
+        }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        File file = (File) o;
+        return id.equals(file.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+        public String toString() {
+            return "File{" +
+                    "id=" + id +
+                    ", fileName='" + fileName + '\'' +
+                    ", contentType='" + contentType + '\'' +
+                    '}';
+        }
+    }
+
