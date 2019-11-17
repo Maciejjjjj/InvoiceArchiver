69493692a01e1219e31480727210d63c819ba1b5
maciej <sucheckimaciej3@gmail.com>
2019-11-17 13:40:46 +0100

    Data base name changed

---
+++ src/main/java/pl/mada/invoice_archiver/config/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+//package pl.mada.invoice_archiver.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//
+//}
