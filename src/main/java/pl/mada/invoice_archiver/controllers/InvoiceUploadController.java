bbf8837c7aa69ee326a86a29263871268794f516
maciej <sucheckimaciej3@gmail.com>
2019-11-21 22:28:00 +0100

    Invoice upload beta vol. 3

--- src/main/java/pl/mada/invoice_archiver/controllers/InvoiceUploadController.java
+++ src/main/java/pl/mada/invoice_archiver/controllers/InvoiceUploadController.java
@@ -1,34 +1,36 @@
-//package pl.mada.invoice_archiver.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.multipart.MultipartFile;
-//import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-//import pl.mada.invoice_archiver.model.entities.File;
-//import pl.mada.invoice_archiver.payload.UploadFileResponse;
-//import pl.mada.invoice_archiver.services.FileStorageService;
-//
-//@Controller
-//@RequestMapping("/upload")
-//public class InvoiceUploadController {
-//
-//    @GetMapping
-//    public String prepareAddInvoicePage() {
-//        return "/WEB-INF/views/invoice-upload.jsp";
-//    }
-//
-//    @PostMapping
-//    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
-//        File file = FileStorageService.storeFile(file);
-//
-//
-//
-//        return new UploadFileResponse(file.getFileName(),
-//                file.getContentType(), file.getData());
-//    }
-//
-//
-//}
+package pl.mada.invoice_archiver.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import pl.mada.invoice_archiver.model.entities.File;
+import pl.mada.invoice_archiver.payload.UploadFileResponse;
+import pl.mada.invoice_archiver.services.FileStorageService;
+
+@Controller
+@RequestMapping("/upload")
+public class InvoiceUploadController {
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @GetMapping
+    public String prepareAddInvoicePage() {
+        return "/WEB-INF/views/invoice-upload.jsp";
+    }
+
+    @PostMapping
+    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+        File fileToSave = fileStorageService.storeFile(file);
+
+
+        return new UploadFileResponse(fileToSave.getFileName(),
+                file.getContentType(), file.getSize());
+    }
+
+
+}
