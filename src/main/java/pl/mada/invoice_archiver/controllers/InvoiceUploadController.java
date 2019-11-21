03df8922ddc198932e739e92557eeb1b5547561d
maciej <sucheckimaciej3@gmail.com>
2019-11-21 21:42:22 +0100

    Invoice upload beta vol. 2

--- src/main/java/pl/mada/invoice_archiver/controllers/InvoiceUploadController.java
+++ src/main/java/pl/mada/invoice_archiver/controllers/InvoiceUploadController.java
@@ -1,11 +1,34 @@
-package pl.mada.invoice_archiver.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/upload")
-public class InvoiceUploadController {
-
-
-}
+//package pl.mada.invoice_archiver.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.multipart.MultipartFile;
+//import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+//import pl.mada.invoice_archiver.model.entities.File;
+//import pl.mada.invoice_archiver.payload.UploadFileResponse;
+//import pl.mada.invoice_archiver.services.FileStorageService;
+//
+//@Controller
+//@RequestMapping("/upload")
+//public class InvoiceUploadController {
+//
+//    @GetMapping
+//    public String prepareAddInvoicePage() {
+//        return "/WEB-INF/views/invoice-upload.jsp";
+//    }
+//
+//    @PostMapping
+//    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
+//        File file = FileStorageService.storeFile(file);
+//
+//
+//
+//        return new UploadFileResponse(file.getFileName(),
+//                file.getContentType(), file.getData());
+//    }
+//
+//
+//}
---
+++ src/main/webapp/WEB-INF/views/invoice-upload.jsp
@@ -0,0 +1,31 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <title>Upload invoice</title>
+</head>
+<body>
+<div class="upload-container">
+    <div class="upload-header">
+        <h2>Upload invoice</h2>
+    </div>
+    <div class="upload-content">
+        <div class="single-upload">
+            <h3>Add invoice to upload</h3>
+            <form id="singleUploadForm" name="singleUploadForm">
+                <input id="singleFileUploadInput" type="file" name="file" class="file-input" required />
+                <button type="submit" class="primary submit-btn">Submit</button>
+            </form>
+            <div class="upload-response">
+                <div id="singleFileUploadError"></div>
+                <div id="singleFileUploadSuccess"></div>
+            </div>
+            <sec:csrfInput/>
+        </div>
+    </div>
+</div>
+</body>
+</html>
