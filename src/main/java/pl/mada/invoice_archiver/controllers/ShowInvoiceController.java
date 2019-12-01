7df8b49394325f94aeca3d26ee992b5c1a2fc648
dannir19 <d.krauz@op.pl>
2019-12-01 13:07:21 +0100

    ocrservice

--- src/main/java/pl/mada/invoice_archiver/controllers/ShowInvoiceController.java
+++ src/main/java/pl/mada/invoice_archiver/controllers/ShowInvoiceController.java
@@ -1,14 +1,12 @@
 package pl.mada.invoice_archiver.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.mada.invoice_archiver.model.entities.Invoice;
 import pl.mada.invoice_archiver.model.repositories.InvoiceRepository;
+
 import java.security.Principal;
 import java.util.List;
 
